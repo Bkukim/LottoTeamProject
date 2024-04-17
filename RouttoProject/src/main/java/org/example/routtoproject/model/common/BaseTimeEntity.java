8edd426c3ce81e1dccf9ceb2b1e58053c7ff35ce
Bku <full1569@gmail.com>
2024-04-17 10:01:50 +0900

    백엔드 파일
    ※ 반드시 압축파일을 푼 키의 경로를 속성에 적어주세여
    
    예) spring.datasource.url=jdbc:log4jdbc:oracle:thin:@orcl_low?TNS_ADMIN=C:/Users/PC/Desktop/team_project/oraclDb/Wallet_orcl/

---
+++ RouttoProject/src/main/java/org/example/routtoproject/model/common/BaseTimeEntity.java
@@ -0,0 +1,52 @@
+package org.example.routtoproject.model.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * packageName : org.example.simpledms.model.common
+ * fileName : BaseTimeEntity
+ * author : GGG
+ * date : 2024-04-02
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-02         GGG          최초 생성
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    private String insertTime;
+
+    private String updateTime;
+
+    //    TODO: JPA 에서 insert 가 실행되기전에 실행하는 함수
+    @PrePersist
+    void OnPrePersist() {
+        this.insertTime = LocalDateTime.now()
+                .format(DateTimeFormatter
+                        .ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    //    TODO: JPA 에서 update 가 실행되기전에 실행되는 함수
+    @PreUpdate
+    void OnPreUpdate() {
+        this.updateTime = LocalDateTime.now()
+                .format(DateTimeFormatter
+                        .ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.insertTime = this.updateTime;
+    }
+}
+
