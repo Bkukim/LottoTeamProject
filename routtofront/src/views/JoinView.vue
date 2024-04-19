8a4a7f5cb06beaa714640e4bad7b07fadded18cb
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-04-19 18:16:12 +0900

    하유정 240419
    OrderView.vue 수정
    AdminManage 생성

--- routtofront/src/views/JoinView.vue
+++ routtofront/src/views/JoinView.vue
@@ -57,7 +57,7 @@
         <!-- 기본 정보 테이블 시작-->
         <table class="table">
           <thead>
-            <tr>
+            <tr >
               <h3 scope="col">기본 정보</h3>
               <th scope="col"></th>
               <th scope="col"></th>
@@ -66,7 +66,7 @@
           </thead>
           <tbody>
             <!-- 아이디 tr -->
-
+            
             <tr>
               <th scope="row">
                 <label class="insert-id" for="id">아이디</label>
@@ -114,7 +114,9 @@
             <!-- 비밀번호 확인 질문 tr -->
             <tr>
               <th scope="row">
-                <label class="form-label" for="">비밀번호 확인 질문</label>
+                <label class="form-label" for="address"
+                  >비밀번호 확인 질문</label
+                >
               </th>
               <td>
                 <select class="form-select" aria-label="Default select example">
@@ -168,23 +170,13 @@
                 <div class="row mb-1">
                   <!-- 우편번호 -->
                   <div class="col">
-                    <input
-                      class="form-control"
-                      type="text"
-                      v-model="postcode"
-                      placeholder="우편번호"
-                      disabled
-                    />
+                    <input class="form-control" type="text" name="address" />
                   </div>
                   <!-- 주소검색 버튼 -->
                   <div class="col">
-                    <input
-                      class="btn"
-                      type="button"
-                      @click="execDaumPostcode()"
-                      value="우편번호 찾기"
-                      id="addressBtn"
-                    />
+                    <button class=" addressBtn btn-sm" type="submit">
+                      주소검색
+                    </button>
                   </div>
                 </div>
                 <div class="row mb-1">
@@ -192,8 +184,8 @@
                     <input
                       class="form-control"
                       type="text"
-                      v-model="address"
-                      placeholder="주소"
+                      name="address"
+                      id="address"
                       disabled
                     />
                   </div>
@@ -203,23 +195,11 @@
                     <input
                       class="form-control"
                       type="text"
-                      v-model="extraAddress"
-                      placeholder=""
-                      disabled
-                    />
-                  </div>
-                </div>
-                <div class="row mb-1">
-                  <div class="col">
-                    <input
-                      class="form-control"
-                      type="text"
-                      id="detailAddress"
+                      name="address"
                       placeholder="상세주소"
                     />
                   </div>
                 </div>
-                
               </td>
               <td></td>
             </tr>
@@ -369,7 +349,11 @@
   <div class="container text-center">
     <div class="row justify-content-md-center">
       <div class="col-md-auto">
-        <button class="text-light singUpBtn btn-sm mt-4" id="" type="submit">
+        <button
+          class="text-light singUpBtn btn-sm mt-4"
+          id=""
+          type="submit"
+        >
           회원가입
         </button>
       </div>
@@ -381,65 +365,16 @@
   <br />
 </template>
 <script>
-export default {
-  data() {
-    return {
-      postcode: "",
-      address: "",
-      extraAddress: "",
-    };
-  },
-  methods: {
-    execDaumPostcode() {
-      new window.daum.Postcode({
-        oncomplete: (data) => {
-          if (this.extraAddress !== "") {
-            this.extraAddress = "";
-          }
-          if (data.userSelectedType === "R") {
-            // 사용자가 도로명 주소를 선택했을 경우
-            this.address = data.roadAddress;
-          } else {
-            // 사용자가 지번 주소를 선택했을 경우(J)
-            this.address = data.jibunAddress;
-          }
-
-          // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
-          if (data.userSelectedType === "R") {
-            // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-            // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-            if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
-              this.extraAddress += data.bname;
-            }
-            // 건물명이 있고, 공동주택일 경우 추가한다.
-            if (data.buildingName !== "" && data.apartment === "Y") {
-              this.extraAddress +=
-                this.extraAddress !== ""
-                  ? `, ${data.buildingName}`
-                  : data.buildingName;
-            }
-            // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-            if (this.extraAddress !== "") {
-              this.extraAddress = `(${this.extraAddress})`;
-            }
-          } else {
-            this.extraAddress = "";
-          }
-          // 우편번호를 입력한다.
-          this.postcode = data.zonecode;
-        },
-      }).open();
-    },
-  },
-};
+export default {};
 </script>
 <style>
-#addressBtn {
+.addressBtn {
   /* 주소 검색 버튼 */
   background-color: #342a26;
   color: white;
 }
 .singUpBtn {
+
   background-color: #342a26;
   color: white;
   font-size: 20px;
---
+++ routtofront/src/views/admin/AdminManage.vue
@@ -0,0 +1,543 @@
+// 관리자 조회/수정 페이지
+<template>
+  <!-- 1. 조회 -->
+  <div class="container">
+    <!-- 1) 조회 배너 시작 -->
+    <div
+      class="container mt-5"
+      style="
+        border: 1px solid black;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 50px;
+      "
+    >
+      <label><b>상품 조회</b></label>
+    </div>
+    <!-- 조회 배너 끝 -->
+
+    <!-- 2) 전체 상태 -->
+    <div
+      class="container mt-4"
+      style="
+        border: 1px solid #342a26;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 200px;
+        flex-direction: row;
+      "
+    >
+      <div class="circle"></div>
+      <div class="text-container">
+        <div>전체</div>
+        <div>0건</div>
+      </div>
+      <div class="circle"></div>
+      <div class="text-container">
+        <div>판매중</div>
+        <div>0건</div>
+      </div>
+      <div class="circle"></div>
+      <div class="text-container">
+        <div>판매중지</div>
+        <div>0건</div>
+      </div>
+      <div class="circle"></div>
+      <div class="text-container">
+        <div>품절</div>
+        <div>0건</div>
+      </div>
+      <div class="circle"></div>
+      <div class="text-container">
+        <div>판매종료</div>
+        <div>0건</div>
+      </div>
+    </div>
+    <!-- 전체 상태 끝-->
+
+    <!-- <br /> -->
+
+    <!-- 3) 검색어 시작 -->
+    <div
+      class="container mt-4"
+      style="
+        border: 1px solid black;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 60px;
+      "
+    >
+      <div
+        style="
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        "
+      >
+        <label id="searchWord">검색어</label>
+
+        <!-- 상품번호 시작 -->
+
+        <div class="form-check" id="prodNum">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+            checked
+          />
+          <label
+            class="form-check-label"
+            for="flexRadioDefault1"
+            id="form-check-label"
+          >
+            상품번호
+          </label>
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" name="prodNum" />
+        </div>
+
+        <!-- 상품번호 끝 -->
+
+        <!-- <label style="align-items: right"> -->
+        <!-- 상품명 시작 -->
+
+        <div class="form-check" id="prodId">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+            checked
+          />
+          <label
+            class="form-check-label"
+            for="flexRadioDefault1"
+            id="form-check-label"
+          >
+            상품명
+          </label>
+        </div>
+        <div class="col">
+          <input class="form-control" type="text" name="prodName" />
+        </div>
+
+        <!-- 상품명 끝 -->
+        <!-- </label> -->
+      </div>
+    </div>
+    <!-- 검색어 끝 -->
+
+    <!-- 4) 판매상태 시작 -->
+    <div
+      class="container mt-4"
+      style="
+        border: 1px solid black;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 60px;
+      "
+    >
+      <div
+        style="
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        "
+      >
+        <label id="salaryStatus"> 판매 상태 </label>
+        <!-- 전체 버튼 -->
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+            checked
+          />
+          <label class="form-check-label" for="flexRadioDefault1" id="status">
+            전체
+          </label>
+        </div>
+
+        <!-- 판매중 버튼 -->
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+            checked
+          />
+          <label class="form-check-label" for="flexRadioDefault1" id="status">
+            판매중
+          </label>
+        </div>
+        <!-- 판매중지 버튼 -->
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+            checked
+          />
+          <label class="form-check-label" for="flexRadioDefault1" id="status">
+            판매중지
+          </label>
+        </div>
+        <!-- 품절 버튼 -->
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+            checked
+          />
+          <label class="form-check-label" for="flexRadioDefault1" id="status">
+            품절
+          </label>
+        </div>
+        <!-- 판매종료 버튼 -->
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="flexRadioDefault"
+            id="flexRadioDefault1"
+            checked
+          />
+          <label class="form-check-label" for="flexRadioDefault1" id="status">
+            판매종료
+          </label>
+        </div>
+      </div>
+    </div>
+    <!-- 판매상태 끝 -->
+
+    <!-- 5) 카테고리 시작 -->
+    <div
+      class="container mt-4"
+      style="
+        border: 1px solid black;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 60px;
+      "
+    >
+      <div
+        style="
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        "
+      >
+        <label id="category"> 카테고리 </label>
+        <label style="align-items: right">
+          <select class="form-select" aria-label="Default select example">
+            <option selected>스킨케어</option>
+            <option value="1">메이크업</option>
+            <option value="2">바디</option>
+            <option value="3">헤어</option>
+          </select>
+        </label>
+      </div>
+    </div>
+    <!-- 카테고리 끝 -->
+  </div>
+
+  <br />
+
+  <!-- 2. 검색/초기화 버튼 -->
+  <div class="container text-center">
+    <button
+      type="button"
+      class="text-light signUpBtn btn-sm mt-4"
+      style="margin-left: 10px"
+    >
+      검색
+    </button>
+    <button
+      type="button"
+      class="text-light signUpBtn btn-sm mt-4"
+      style="margin-left: 10px"
+    >
+      초기화
+    </button>
+  </div>
+
+  <!-- 3. 수정 페이지 -->
+  <div class="container">
+    <!-- 1) 수정 배너 시작 -->
+    <div
+      class="container mt-5"
+      style="
+        border: 1px solid black;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 50px;
+      "
+    >
+      <label><b>상품 수정</b></label>
+    </div>
+    <!-- 수정 배너 끝 -->
+
+    <br />
+
+    <!-- 2) 상품 항목 시작 -->
+    <div>
+      <!-- 기본 정보 테이블 시작-->
+      <table class="table table-bordered" style="border: 1px solid #342a26">
+        <!-- 테이블 제목 행 -->
+        <thead>
+          <tr>
+            <th scope="col">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
+            </th>
+            <th scope="col">수정</th>
+            <th scope="col">상품번호</th>
+            <th scope="col">상품명</th>
+            <th scope="col">상세설명</th>
+            <th scope="col">판매상태</th>
+            <th scope="col">재고수량</th>
+            <th scope="col">판매가</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- 1행 -->
+          <tr>
+            <th scope="row">
+              <label class="form-label" for="user"
+                ><input
+                  class="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+              /></label>
+            </th>
+            <td>
+              <button
+                type="button"
+                class="text-light updateBtn btn btn-success">
+                수정
+              </button>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+          </tr>
+          <!-- 2행 tr -->
+          <tr>
+            <th scope="row">
+              <label class="form-label" for="user"
+                ><input
+                  class="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+              /></label>
+            </th>
+            <td>
+              <button
+                type="button"
+                class="text-light updateBtn btn btn-success">
+                수정
+              </button>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+          </tr>
+          <!-- 3행 tr -->
+          <tr>
+            <th scope="row">
+              <label class="form-label" for="user"
+                ><input
+                  class="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckDefault"
+              /></label>
+            </th>
+            <td>
+              <button
+                type="button"
+                class="text-light updateBtn btn btn-success">
+                수정
+              </button>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+            <td>
+              <div class="col-6">
+                <input class="form-control" type="text" name="user" />
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- 상품 항목 끝 -->
+  </div>
+  <!-- 4. 수정 항목 저장 버튼 -->
+  <div class="container text-center">
+    <button
+      type="button"
+      class="text-light signUpBtn btn-sm mt-4"
+      style="margin-left: 10px"
+    >
+      수정 항목 저장
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.searchBtn {
+  background-color: #342a26;
+  color: white;
+}
+
+.circle {
+  width: 2.94vw;
+  height: 2.94vw;
+  background-color: #342a26; /* 동그라미의 색상 */
+  border-radius: 50%; /* 원 모양으로 만들기 위해 50%로 설정 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.text-container {
+  text-align: center;
+}
+
+#salaryStatus {
+  margin-right: 6.94vw;
+}
+
+#searchWord {
+  margin-right: 8vw;
+}
+
+/* #prodNum {
+  margin-right: 6vw;
+} */
+
+#status {
+  margin-right: 7vw;
+}
+
+#form-check-label {
+  margin-right: 2vw;
+}
+
+#prodId {
+  margin-left: 10vw;
+}
+
+.signUpBtn {
+  background-color: #342a26;
+  color: white;
+  font-size: 20px;
+  width: 200px;
+  height: 50px;
+}
+
+#category {
+  margin-right: 7vw;
+}
+</style>
