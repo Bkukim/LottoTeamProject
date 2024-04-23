1f9b8ded7834ee91c3fa3bf94599776f67be042d
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-04-23 09:26:27 +0900

    Update ProductService.java

--- RouttoProject/src/main/java/org/example/routtoproject/service/shop/ProductService.java
+++ RouttoProject/src/main/java/org/example/routtoproject/service/shop/ProductService.java
@@ -9,11 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import org.springframework.web.bind.annotation.RequestParam;
-
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.util.Optional;
 import java.util.UUID;
 
@@ -43,7 +38,7 @@ public class ProductService {
     }
 
 
-        // 상품 저장함수
+    // 상품 저장함수
 
     public Product save(String prodName,
                         int defaultPrice,
@@ -58,44 +53,41 @@ public class ProductService {
         Product product2 = null;
         try {
 
-            if (prodImgUrl == null && prodDetailPageUrl == null) {
+            if (prodImgUrl == null &&  prodDetailPageUrl == null) {
+                // todo : 기본키가 없을때 : insert
+                //      1-1) uuid 생성하기
+                String prodImgUuid = UUID.randomUUID().toString().replace("-", ""); // uuid 만드는 방법T
+                String prodDetailPageUuid = UUID.randomUUID().toString().replace("-", ""); // uuid 만드는 방법
+                // xxxx-xxxx-xxxx-xx...이런 형태로 만들어진다. 근데 "-"가 보기 좋지 않으니 없애보자. replace 함수 이용
 
-                if (prodImgUrl == null &&  prodDetailPageUrl == null) {
+                // todo  1-2) 다운로드 url 생성 -> 자바함수를 이용 ※여기서 다운로드란 jsp이 spring에서 이미지를 다운받아 가져오는 것.
+                String prodImgDownload = ServletUriComponentsBuilder
+                        .fromCurrentContextPath()// 스프링 서버 기본 주소 : localhost:8000
+                        .path("/api/shop/product/") // 추가 경로 넣기 : /advanced/fileDb
+                        .path(prodImgUuid) // uuid를 url 제일 마지막에 넣어주기
+                        .toUriString(); // 위의 url을 하나로 합쳐주는 함수 http://localhost:8000/advanced/fileDb/xxxx 가 된다.
+                String prodDetailPageDownload = ServletUriComponentsBuilder
+                        .fromCurrentContextPath()// 스프링 서버 기본 주소 : localhost:8000
+                        .path("/api/shop/product/") // 추가 경로 넣기 : /advanced/fileDb
+                        .path(prodDetailPageUuid) // uuid를 url 제일 마지막에 넣어주기
+                        .toUriString(); // 위의 url을 하나로 합쳐주는 함수 http://localhost:8000/advanced/fileDb/xxxx 가 된다.
 
-                    // todo : 기본키가 없을때 : insert
-                    //      1-1) uuid 생성하기
-                    String prodImgUuid = UUID.randomUUID().toString().replace("-", ""); // uuid 만드는 방법T
-                    String prodDetailPageUuid = UUID.randomUUID().toString().replace("-", ""); // uuid 만드는 방법
-                    // xxxx-xxxx-xxxx-xx...이런 형태로 만들어진다. 근데 "-"가 보기 좋지 않으니 없애보자. replace 함수 이용
-
-                    // todo  1-2) 다운로드 url 생성 -> 자바함수를 이용 ※여기서 다운로드란 jsp이 spring에서 이미지를 다운받아 가져오는 것.
-                    String prodImgDownload = ServletUriComponentsBuilder
-                            .fromCurrentContextPath()// 스프링 서버 기본 주소 : localhost:8000
-                            .path("/api/shop/product/") // 추가 경로 넣기 : /advanced/fileDb
-                            .path(prodImgUuid) // uuid를 url 제일 마지막에 넣어주기
-                            .toUriString(); // 위의 url을 하나로 합쳐주는 함수 http://localhost:8000/advanced/fileDb/xxxx 가 된다.
-                    String prodDetailPageDownload = ServletUriComponentsBuilder
-                            .fromCurrentContextPath()// 스프링 서버 기본 주소 : localhost:8000
-                            .path("/api/shop/product/") // 추가 경로 넣기 : /advanced/fileDb
-                            .path(prodDetailPageUuid) // uuid를 url 제일 마지막에 넣어주기
-                            .toUriString(); // 위의 url을 하나로 합쳐주는 함수 http://localhost:8000/advanced/fileDb/xxxx 가 된다.
-
-                    // todo  1-3) 생성자에 만든 url넣어주기
-                    Product product = new Product(prodName,
-                            defaultPrice,
-                            prodCategory,
-                            prodImg.getBytes(), // 파일 데이터
-                            prodDetailPage.getBytes(), // 파일 데이터
-                            discountRate,
-                            prodStock,
-                            prodImgDownload,
-                            prodDetailPageDownload); // 우리가 만든 url
-                    product2 = productRepository.save(product);
-                }
-            } catch (Exception e) {
-                log.debug(e.getMessage());
-
+                // todo  1-3) 생성자에 만든 url넣어주기
+                Product product = new Product(prodName,
+                        defaultPrice,
+                        prodCategory,
+                        prodImg.getBytes(), // 파일 데이터
+                        prodDetailPage.getBytes(), // 파일 데이터
+                        discountRate,
+                        prodStock,
+                        prodImgDownload,
+                        prodDetailPageDownload); // 우리가 만든 url
+                product2 = productRepository.save(product);
             }
-            return product2;
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+
         }
+        return product2;
     }
+}
