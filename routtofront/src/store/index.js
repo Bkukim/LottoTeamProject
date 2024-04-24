be18c7a29593082601f6e4b1d9770b8f778d4dbc
BALLBAT <159212526+BALLBAT@users.noreply.github.com>
2024-04-24 17:56:53 +0900

    Update index.js

--- routtofront/src/store/index.js
+++ routtofront/src/store/index.js
@@ -12,7 +12,14 @@ export default createStore({
     loggedIn: user ? true : false, // 로그인 여부
     user: user ? user : null, //  로그인 정보 (웹토큰 속성 있음)
 
+<<<<<<< Updated upstream
     selectedOption: "", // 라디오 버튼에서 선택된 옵션
+=======
+    selectedOption: '', // 라디오 버튼에서 선택된 옵션
+
+    userId:"김의현",
+  }, 
+>>>>>>> Stashed changes
 
     userId: "김의현",
   },
