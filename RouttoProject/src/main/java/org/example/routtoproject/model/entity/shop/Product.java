badc85b0d7a4b5d5234ab4eb77fe7b70a8e7c435
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-04-25 20:47:26 +0900

    하유정 240425
    주문페이지 백엔드 : order 엔티티, orderProd 엔티티, orderDto, save함수 생성, 프론트 : 바인딩 속성 작성

--- RouttoProject/src/main/java/org/example/routtoproject/model/entity/shop/Product.java
+++ RouttoProject/src/main/java/org/example/routtoproject/model/entity/shop/Product.java
@@ -6,7 +6,9 @@ import org.example.routtoproject.model.common.BaseTimeEntity;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * packageName : org.example.routtoproject.model.entity.shop
@@ -43,20 +45,21 @@ public class Product extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.SEQUENCE
             , generator = "SQ_LOTTO_PRODUCT_GENERATOR"
     )
-    private Integer prodId;     // 기본키, 시퀀스, 상품id
-    private String prodName;
-    private Integer defaultPrice;
-    private String prodCategory;
-    private byte[] prodImg;
-    private byte[] prodDetailPage;
-    private Integer discountRate;
-    private Integer prodStock;
-    private String prodStatus;
-    private Integer soldCount;
-    private String prodImgUrl;
-    private String prodDetailPageUrl;
-    private String prodImgUuid;
-    private String prodDetailPageUuid;
+    private Integer prodId;       // 기본키, 시퀀스, 상품id
+    private String prodName;      // 상품 이름
+    private Integer defaultPrice; // 원가
+    private String prodCategory;  // 카테고리
+    private byte[] prodImg;       // 상품 이미지
+    private byte[] prodDetailPage; // 상품 상세 페이지
+    private Integer discountRate; // 할인율
+    private Integer prodStock;    // 재고
+    private String prodStatus;    // 상품 판매 상태
+    private Integer soldCount;    // 상품 판매 횟수
+    private String prodImgUrl;     // 상품 이미지 url
+    private String prodDetailPageUrl; // 상품 상세 이미지 url
+    private String prodImgUuid;       // 상품 이미지 uuid
+    private String prodDetailPageUuid; // 상품 상세 이미지 uuid
+
 
     public Product(String prodName, Integer defaultPrice, String prodCategory, byte[] prodImg, byte[] prodDetailPage, Integer discountRate, Integer prodStock, String prodImgUrl, String prodDetailPageUrl, String prodImgUuid, String prodDetailPageUuid) {
         this.prodName = prodName;
--- RouttoProject/src/main/java/org/example/routtoproject/service/shop/OrderService.java
+++ RouttoProject/src/main/java/org/example/routtoproject/service/shop/OrderService.java
@@ -2,9 +2,16 @@ package org.example.routtoproject.service.shop;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.routtoproject.model.dto.shop.OrderDto;
 import org.example.routtoproject.model.entity.shop.Order;
+import org.example.routtoproject.model.entity.shop.OrderProd;
+import org.example.routtoproject.repository.shop.OrderProdRepository;
 import org.example.routtoproject.repository.shop.OrderRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 /**
  * packageName : org.example.routtoproject.service.shop
@@ -23,11 +30,40 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class OrderService {
-    private final OrderRepository orderRepository;
 
-//    TODO: 저장 함수
-    public Order save(Order order){
-        Order order1 = orderRepository.save(order);
-        return order1;
+    private final OrderRepository orderRepository;
+    private final OrderProdRepository orderProdRepository;
+
+    ModelMapper modelMapper = new ModelMapper();
+//    TODO: 주문/결제 페이지에서 주문 정보 입력시 DB에 저장하는 함수
+
+    @Transactional
+    public Order saveOrder(OrderDto orderDto) {
+//        TODO: 1) insert 할 때는 DTO -> Entity 로 변환해서 insert
+//              (1) 직접 변환로직 코딩하기 : 숙련자 추천
+//              (2) 자동 변환 패키지 : 초보자 추천 (ModelMapper 패키지 : 성능 저하)
+//      simpleOrderRepository.save();  // save 함수의 매개변수에는 entity 를 넣어야하는데 Dto를 쓸수없으니 entity로 변환해야함
+
+//        TODO: 1) DTO -> Entity로 변환
+//         사용법 : modelMapper.map(DTO 클래스, 엔티티명.class);
+
+        Order order = modelMapper.map(orderDto, Order.class);
+
+//        TODO: 2) 부모테이블 저장(부모저장 후 자식저장)
+        Order order2 =orderRepository.save(order);
+
+//        TODO: 3) 자식테이블도 저장(배열이라서 반복문 돌리기) : 주문상세 테이블(반복문)
+//                 DB 트랜잭션(transaction, 거래) :
+//                  (1) CUD 작업에 대해 여러개가 있을 경우 중간에 에러가 발생하면 모두 롤백함
+//                  (2) 위의 있는 기능을 사용하려면 : @Transaction 함수위에 붙임
+        for (int i = 0; i < orderDto.getOrderProds().size(); i++) {
+//            자식테이블 insert : 기본키(부모쪽 insert 할 때 시퀀스로 생성되어 있음)
+//            생성된 주문번호 -> 주문상세객체에 저장
+            OrderProd tmpOrderProd = orderDto.getOrderProds().get(i);
+            tmpOrderProd.setOrderId(order2.getOrderId());
+//            DB 저장
+            orderProdRepository.save(tmpOrderProd);
+        }
+        return order2;    // 저장된 주문 객체
     }
 }
