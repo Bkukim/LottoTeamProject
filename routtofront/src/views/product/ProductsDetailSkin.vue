b529cb07c27a8f38b13dc0d28ddb5a1dcc8377e0
Bku <full1569@gmail.com>
2024-05-14 11:00:05 +0900

    0514 김의현
    상품 조회 만듬

--- routtofront/src/views/product/ProductsDetailSkin.vue
+++ routtofront/src/views/product/ProductsDetailSkin.vue
@@ -2,12 +2,11 @@
   <!-- 전체 중앙정렬 -->
   <div class="main_all container text-left">
     <div class="mb-3">
-      <h3 class="mt-1">SKIN</h3>
+      <h3 class="mt-1">{{ category }}</h3>
       <hr />
-      <br>
-      <br>
+      <br />
+      <br />
     </div>
-
 
     <!-- 메인페이지 상품바로가기 반복문 -->
     <div class="product-wrapper container text-center">
@@ -45,7 +44,7 @@
   </div>
   <!-- 전체 중앙정렬 끝 -->
 </template>
-  <script>
+<script>
 import AdminAdBannerService from "@/services/admin/AdminAdBannerService";
 import ProductService from "@/services/product/ProductService";
 
@@ -55,37 +54,67 @@ export default {
       // prodImgUrl:["http://localhost:8000/api/normal/shop/product/img/3e89a3876b87444a88965fb0a2f2ba8f", "http://localhost:8000/api/normal/shop/product/img/5b456b748e944e63bdb4591b20c2403b"],
       banner: [],
       product: [],
-      searchTitle: "", // 검색어
 
-      // currentSlideIndex: 0,
-      // slides: document.querySelectorAll('.slide') ,// 슬라이드 요소들을 선택합니다.
+      category: "",
 
       // 공통 페이징 속성
       page: 1, // 현재페이지번호
       count: 0, // 전체 데이터개수
-      pageSize: 3, // 화면에 보여질 개수
-
-      pageSizes: [3, 6, 9], // 화면에 보여질 개수배열
+      pageSize: 5, // 화면에 보여질 개수
     };
   },
 
   methods: {
+    chooseCategory(category) {
+      if (category == 1) {
+        this.category = "SKIN";
+      } else if (category == 2) {
+        this.category = "HAIR";
+      } else if (category == 3) {
+        this.category = "MAKE UP";
+      } else if (category == 4) {
+        this.category = "BODY";
+      } else if (category == 0) {
+        this.category = "BEST";
+      }
+    },
+
     async getAllBanner() {
       try {
-        let response = await AdminAdBannerService.findAllBannerHome();
-        this.banner = response.data;
+        let response = await AdminAdBannerService;
         console.log("배너 ::: ", response.data);
       } catch (error) {
         console.log(error);
       }
     },
     // TODO: 전체조회 함수    : 검색어 버튼 태그
-    async retrieveSimpleProduct() {
+    async retrieveProductByCategory(category) {
       try {
-        // TODO: 공통 전체조회 서비스 함수 실행
-        // TODO: spring 통신 : 비동기 코딩 : async ~ await
-        let response = await ProductService.getAll();
-        this.product = response.data; // spring 전달 객체배열
+        let prodCategory = "";
+        if (category == 1) {
+          prodCategory = "스킨케어";
+        } else if (category == 2) {
+          prodCategory = "헤어";
+        } else if (category == 3) {
+          prodCategory = "메이크업";
+        } else if (category == 4) {
+          prodCategory = "바디";
+        } else if (category == 0) {
+          let response = await ProductService.getBestProduct();
+          console.log(response.data);
+          this.product = response.data.product;
+          this.count = response.data.totalItems; // spring 전달 객체배열
+          return;
+        }
+
+        let response = await ProductService.getProductByCategory(
+          prodCategory,
+          this.page - 1,
+          this.pageSize
+        );
+        console.log(response.data);
+        this.product = response.data.product; // spring 전달 객체배열
+        this.count = response.data.totalItems; // spring 전달 객체배열
         // 전체페이지개수
         // 로깅(디버깅)
         // console.log(response.data); // 웹브라우저 콘솔탭에 spring 전달 객체배열이 표시됨
@@ -102,17 +131,25 @@ export default {
   //   TODO: 화면이 뜰때 자동 실행 함수
   mounted() {
     // TODO: 전체 조회 자동 실행
+    this.chooseCategory(this.$route.params.prodCategory);
     this.getAllBanner();
-    this.retrieveSimpleProduct();
+    this.retrieveProductByCategory(this.$route.params.prodCategory);
     window.scrollTo(0, 0);
+  },
+  watch: {
+    $route(to, form) {
+      if (to.path !== form.path) {
+        this.retrieveProductByCategory(this.$route.params.prodCategory);
+        this.chooseCategory(this.$route.params.prodCategory);
+      }
+    },
   },
 };
 </script>
-  <style>
+<style>
 /* best 상품 반복문 가로 정렬 */
 .product-wrapper {
   display: flex;
   gap: 30px;
 }
 </style>
-  
