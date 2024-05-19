c423f1ecd07d369b76ac79a940cd67bb6a44588f
wldudrhdwnsla99 <165975204+wldudrhdwnsla99@users.noreply.github.com>
2024-05-20 08:53:35 +0900

    검색창

--- routtofront/src/views/search/SearchProductView.vue
+++ routtofront/src/views/search/SearchProductView.vue
@@ -1,92 +1,74 @@
 <template>
-
   <div class="container text-center">
-    <div>
-      <h3 class="search_title mt-5">Routto Search</h3>
+    <h3 class="search_title">Routto Search</h3>
 
-      <div class="container text-center">
-        <!-- Stack the columns on mobile by making one full-width and the other half-width -->
-        <div class="row">
-          <div class="col-md-4">
-            <h5>SEARCH</h5>
-            <P>키워드를 입력하여 원하시는 상품을 찾아보세요.</P>
-            <div class="search_input">
-              <input
-                type="text"
-                class="form-control"
-                id="prodName"
-                placeholder="prodName"
-                name="prodName"
-                v-model="searchProduct"
-              />
+    <div class="search_input mt-5 ">
+      <input
+        type="text"
+        class="form-control"
+        id="prodName"
+        placeholder="prodName"
+        name="prodName"
+        v-model="searchProduct"
+      />
+      <div @click="retrieveProduct">
+        <img class="search" src="@/assets/images/search.png" />
+      </div>
+    </div>
+    <p class="mt-3 mb-5">키워드를 입력하여 원하시는 상품을 찾아보세요.</p>
 
-              <div @click="retrieveProduct">
-                <img class="search" src="@/assets/images/search.png" />
-              </div>
-            </div>
-          </div>
- <!-- 메인페이지 상품바로가기 반복문 -->
-    <div class="product-wrapper container text-center">
-      <div
-        class="product2_in image-kim"
-        v-for="(data, index) in products"
-        :key="index"
-      >
+    <!-- 메인페이지 상품바로가기 반복문 -->
+    <div class="product-wrapper">
+      <div class="product2_in" v-for="(data, index) in products" :key="index">
         <!-- 상품이미지 -->
-        <router-link :to="'/product/' + data.prodId"
-          ><img
+        <router-link :to="'/product/' + data.prodId">
+          <img
             class="frame-11"
             :src="data.prodImgUrl"
             style="max-width: 400px; max-height: 400px"
-        /></router-link>
-
-        <div class="abcdefghijklm">
+          />
+        </router-link>
+        <!-- 상품 명 -->
+        <div class="product-name">
           <p>{{ data.prodName }}</p>
         </div>
-
-
-        <div class="abcdef">
+        <!-- 상품 가격 -->
+        <div class="product-price">
           {{ (data.defaultPrice * (100 - data.discountRate)) / 100 + " won" }}
         </div>
-        <!-- </div> -->
       </div>
     </div>
     <!-- 메인페이지 상품바로가기 반복문 끝-->
-          <div class="col-md-8">
-            <div class="row" v-for="(data, index) in products" :key="index">
-              <div class="col-md-4 mr-4">
-                <div class="serch_box"><img :src="data.prodImgUrl"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
-  <script>
-  import ProductService from '@/services/product/ProductService';
+
+<script>
+import ProductService from "@/services/product/ProductService";
 export default {
   data() {
     return {
-      products:[],
-      searchProduct:"",
-
+      products: [],
+      searchProduct: "",
       page: 1, // 현재페이지번호
       count: 0, // 전체 데이터개수
-      pageSize: 3, // 화면에 보여질 개수
-    }
-  },methods: {
-   async retrieveProduct(){
+      pageSize: 12, // 화면에 보여질 개수
+    };
+  },
+  methods: {
+    async retrieveProduct() {
       try {
-         let response = await ProductService.getSearchProductAll(this.searchProduct,this.page-1,this.pageSize)
+        let response = await ProductService.getSearchProductAll(
+          this.searchProduct,
+          this.page - 1,
+          this.pageSize
+        );
         console.log(response.data);
         this.products = response.data.products;
         this.count = response.data.totalItems;
       } catch (error) {
         console.log(error);
       }
-    }
+    },
   },
   mounted() {
     this.retrieveProduct();
@@ -94,61 +76,59 @@ export default {
   },
 };
 </script>
-  <style>
-.container {
 
-min-height: 1000px;
-height: auto;
-  /* margin-top: 250px; */
+<style>
+.container {
+  height: auto;
 }
 .search {
-  position: absolute;
-  top: -40.7202%; /* 헤더 높이에 따른 위치 조정 */
-  left: 13vw;
+  cursor: pointer;
 }
 h3 {
-  /* margin-top: 10px; */
-  margin-bottom: 80px;
-}
-h5{
-  position: absolute;
-  top: 50.7202%; /* 헤더 높이에 따른 위치 조정 */
-  left: 16vw;
-}
-p{
-  position: absolute;
-  top: 54.7202%; /* 헤더 높이에 따른 위치 조정 */
-  left: 16vw;
+  margin-bottom: 20px;
 }
 .search_input {
   width: 250px;
+  margin: 0 auto;
+  position: relative;
+}
+.search_input img {
   position: absolute;
-  top: 60.7202%; /* 헤더 높이에 따른 위치 조정 */
-  left: 16vw;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  cursor: pointer;
 }
-
-
-.serch_box {
-  background-color: #342a26;
-  width: 300px;
-  height: 500px;
-  color: #ffffff;
-}
-
-.col-md-4 {
-  margin-right: 50px;
-}
-
-.row {
-  --bs-gutter-x: 1.5rem;
-  --bs-gutter-y: 0;
+.product-wrapper {
   display: flex;
-  flex-wrap: nowrap;
-  margin-top: calc(-1 * var(--bs-gutter-y));
-  margin-right: calc(-0.5 * var(--bs-gutter-x));
-  margin-left: calc(-0.5 * var(--bs-gutter-x));
-  align-items: center;
-  justify-content: space-around;
+  flex-wrap: wrap;
+  gap: 50px;
+  justify-content: center;
+  margin-top: 80px;
+}
+.product2_in {
+  flex: 0 1 calc(33.33% - 50px);
+  box-sizing: border-box;
+  margin-bottom: 30px;
+}
+.product-name,
+.product-price {
+  margin-top: 10px;
+}
+
+.product-name > p{
+  color: #535353;
+  font-family: "Inter-Light", sans-serif;
+  font-size: 17px;
+  font-weight: 700;
+  letter-spacing: -1.5px;
+}
+
+.product-price{
+  color: #535353;
+  font-family: "Inter-Light", sans-serif;
+  font-size: 17px;
+  font-weight: 300;
+  letter-spacing: -1.5px;
 }
 </style>
-  
